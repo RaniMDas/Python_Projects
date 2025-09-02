aa2a36206fed3f427c03862a84c63bd7c4b6f745
Rani M Das <rani.mdas@gmail.com>
2025-09-02 23:16:17 +0530

    Add my files

---
+++ filess/file.py
@@ -0,0 +1,8 @@
+import os
+print(os.getcwd())
+f1=open(r"C:\Users\ranit\OneDrive\Desktop\A11 Python DSA\filess\nhc.txt",'r')
+
+#directory = locates the files
+#mode=read,write,append,readandwrirte
+f1.close()
+
